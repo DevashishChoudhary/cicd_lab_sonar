a2f9bc7929a4415995ba8cf6c240c85fe7767709
DevashishChoudhary <isshuchoudhary99@gmail.com>
2020-08-18 13:01:16 +0530

    Second Commit

--- DevOps.Sonar.Test/src/main/java/Sonar_1.java
+++ DevOps.Sonar.Test/src/main/java/Sonar_1.java
@@ -1,4 +1,19 @@
-
+class number {
+	private int n=10;
+	void Num(int n1) {
+		n1=10;
+	}
+}
 public class Sonar_1 {
+public static void main (String args[])
+{
+	int n2;
+	System.out.println("Hello, Welcome to the Sonar lab session");
 
+if(n2==20)
+{
+	System.out.println("False statement");
+}
+
+}
 }
